e65ae9c01ce86bfb6ad12c3f9add5cafe470cbd8
Arthur Eichmann <aarrtthhuurr@hotmail.de>
2021-01-12 19:52:52 +0100

    Projektdateien hinzufügen.

---
+++ Kalender/Calendar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueoChallengeImplementation
+{
+    public static class Calendar
+    {
+        public static string[,] GetYearSheet(int year)
+        {
+
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+        private static int CountDaysMonth(int year, int month)
+        {
+            int days = System.DateTime.DaysInMonth(year, month);
+            return days;
+        }
+
+    }
+}
